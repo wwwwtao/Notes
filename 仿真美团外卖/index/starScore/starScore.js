42a5e462f7d9723a0d2e6341168cdac00d009a1e
wwwwtao <934725827@qq.com>
2019-05-18 09:41:43 +0800

    仿真美团外卖

---
+++ 仿真美团外卖/index/starScore/starScore.js
@@ -0,0 +1,15 @@
+(function () {
+
+    //得分模板字符串
+    var itemTmpl = '<div class="star-score">$starstr</div>';
+
+    function _getStars(){
+
+    }
+
+    window.StarScore = function(score){
+        
+
+    }
+
+})();
--- 仿真美团外卖/首页源码/index/contentList/contentList.js
+++ 仿真美团外卖/首页源码/index/contentList/contentList.js
@@ -1,25 +1,25 @@
-(function(){
+(function () {
 
     // 商家详情模版字符串
-    var itemTmpl = '<div class="r-item-content">'+
-                        '<img class="item-img" src=$pic_url />'+
-                        '$brand'+
-                        '<div class="item-info-content">'+
-                            '<p class="item-title">$name</p>'+
-                            '<div class="item-desc clearfix">'+
-                                '<div class="item-score">$wm_poi_score</div>'+
-                                '<div class="item-count">月售$monthNum</div>'+
-                                '<div class="item-distance">&nbsp;$distance</div>'+
-                                '<div class="item-time">$mt_delivery_time&nbsp;|</div>'+
-                            '</div>'+
-                            '<div class="item-price">'+
-                                '<div class="item-pre-price">$min_price_tip</div>'+
-                            '</div>'+
-                            '<div class="item-others">'+
-                                '$others'+
-                            '</div>'+
-                        '</div>'+
-                    '</div>';
+    var itemTmpl = '<div class="r-item-content">' +
+        '<img class="item-img" src=$pic_url />' +
+        '$brand' +
+        '<div class="item-info-content">' +
+        '<p class="item-title">$name</p>' +
+        '<div class="item-desc clearfix">' +
+        '<div class="item-score">$wm_poi_score</div>' +
+        '<div class="item-count">月售$monthNum</div>' +
+        '<div class="item-distance">&nbsp;$distance</div>' +
+        '<div class="item-time">$mt_delivery_time&nbsp;|</div>' +
+        '</div>' +
+        '<div class="item-price">' +
+        '<div class="item-pre-price">$min_price_tip</div>' +
+        '</div>' +
+        '<div class="item-others">' +
+        '$others' +
+        '</div>' +
+        '</div>' +
+        '</div>';
 
     var page = 0;
     var isLoading = false;
@@ -27,17 +27,17 @@
     *  获取商家列表数据
     *  param 
     */
-    function getList(){
+    function getList() {
         page++;
         isLoading = true;
-        $.get('../json/homelist.json', function(data){
+        $.get('../json/homelist.json', function (data) {
 
             console.log(data);
             var list = data.data.poilist || [];
 
             initContentList(list);
             isLoading = false;
-            
+
         });
     }
 
@@ -45,7 +45,7 @@
     *  渲染是否时新到热门品牌标签
     *  param {} data
     */
-    function getBrand(data){
+    function getBrand(data) {
         if (data.brand_type) {
             return '<div class="brand brand-pin">品牌</div>';
         } else {
@@ -57,7 +57,7 @@
     *  渲染月售
     *  param {} data
     */
-    function getMonthNum(data){
+    function getMonthNum(data) {
         var num = data.month_sale_num;
 
         // 大于999采用999+
@@ -72,24 +72,24 @@
     *  渲染商家活动
     *  param {} data
     */
-    function getOthers(data){
+    function getOthers(data) {
 
         var array = data.discounts2;
 
         var str = '';
 
-        array.forEach(function(item, index){
+        array.forEach(function (item, index) {
 
             // 内部的商家活动模版字符串
-            var _str = '<div class="other-info">'+
-                           '<img src=$icon_url class="other-tag" />'+
-                           '<p class="other-content one-line">$info</p>'+
-                        '</div>';
+            var _str = '<div class="other-info">' +
+                '<img src=$icon_url class="other-tag" />' +
+                '<p class="other-content one-line">$info</p>' +
+                '</div>';
 
 
             // 模版字符串替换数据
-            _str = _str.replace('$icon_url',item.icon_url)
-                        .replace('$info', item.info);
+            _str = _str.replace('$icon_url', item.icon_url)
+                .replace('$info', item.info);
 
 
             // 字符串拼接
@@ -103,23 +103,23 @@
     *  渲染列表数据
     *  param []
     */
-    function initContentList(list){
-        list.forEach(function(item, index){
+    function initContentList(list) {
+        list.forEach(function (item, index) {
             var str = itemTmpl
-                      .replace('$pic_url',item.pic_url)
-                      .replace('$name',item.name)
-                      .replace('$distance',item.distance)
-                      .replace('$min_price_tip',item.min_price_tip)
-                      .replace('$mt_delivery_time',item.mt_delivery_time)
+                .replace('$pic_url', item.pic_url)
+                .replace('$name', item.name)
+                .replace('$distance', item.distance)
+                .replace('$min_price_tip', item.min_price_tip)
+                .replace('$mt_delivery_time', item.mt_delivery_time)
 
-                      .replace('$brand',getBrand(item))
+                .replace('$brand', getBrand(item))
 
-                      .replace('$monthNum',getMonthNum(item))
+                .replace('$monthNum', getMonthNum(item))
 
-                      .replace('$others',getOthers(item))
+                .replace('$others', getOthers(item))
 
-                      .replace('$wm_poi_score', new StarScore(item.wm_poi_score).getStars())
-                      
+                .replace('$wm_poi_score', new StarScore(item.wm_poi_score).getStars())
+
 
             $('.list-wrap').append($(str));
         })
@@ -127,15 +127,15 @@
     }
 
 
-    function addEvent(){
-        window.addEventListener('scroll',function(){
+    function addEvent() {
+        window.addEventListener('scroll', function () {
             var clientHeight = document.documentElement.clientHeight;
             var scrollHeight = document.body.scrollHeight;
             var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
 
             var proDis = 30;
-            if ((scrollTop + clientHeight) >= (scrollHeight-proDis)) {
-                
+            if ((scrollTop + clientHeight) >= (scrollHeight - proDis)) {
+
 
                 // 最多滚动加载3页
                 if (page < 3) {
@@ -145,11 +145,11 @@
                         return;
                     }
                     getList();
-                }else {
+                } else {
                     $('.loading').text('加载完成');
                 }
 
-                
+
             }
         });
     }
