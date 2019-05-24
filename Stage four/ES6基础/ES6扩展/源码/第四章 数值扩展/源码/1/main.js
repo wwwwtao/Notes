ab025b5873237dcc93a410368e20cb47baffe86e
wwwwtao <934725827@qq.com>
2019-05-24 22:33:28 +0800

    ES6

--- Stage four/ES6基础/ES6扩展/源码/第四章 数值扩展/源码/1/main.js
+++ Stage four/ES6基础/ES6扩展/源码/第四章 数值扩展/源码/1/main.js
@@ -2,9 +2,8 @@
 
 // 016  =>  14
 
-// 0o  0O    octonary
-// 0b  0B    binary
+// 0o  0O    octonary   八进制
+// 0b  0B    binary     二进制
 
-// console.log(0o16);
-// console.log(0b1111);
- 
+// console.log(0o16);   14
+// console.log(0b1111); 15 
