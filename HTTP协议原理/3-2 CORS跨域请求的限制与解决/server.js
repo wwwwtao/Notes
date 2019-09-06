3bff6e7c0f59726a24c2688538ac57fa651ed1f8
wwwwtao <934725827@qq.com>
2019-09-06 16:10:17 +0800

    缓存头 Cache-Control 的含义和使用 （客户端缓存）

--- HTTP协议原理/3-2 CORS跨域请求的限制与解决/server.js
+++ HTTP协议原理/3-2 CORS跨域请求的限制与解决/server.js
@@ -3,14 +3,26 @@
 const http = require('http')
 const fs = require('fs')
 
-http.createServer(function(request,response){
-  console.log('request come',request.url)
+http.createServer(function (request, response) {
+  console.log('request come', request.url)
 
-  const html = fs.readFileSync('test.html','utf-8')
-  response.writeHead(200,{
-    'Content-type': 'text/html'
-  })
-  response.end(html)
-}).listen(8888)
+  if (request.url === '/') {
+    const html = fs.readFileSync('test.html', 'utf-8')
+    response.writeHead(200, {
+      'Content-type': 'text/html'
+    })
+    response.end(html)
+  }
 
-console.log('server listening on 8888')
+  if (request.url === '/script.js') {
+    const html = fs.readFileSync('test.html', 'utf-8')
+    response.writeHead(200, {
+      'Content-type': 'text/javascript',
+      'Cache-Control': 'max-age=200,public'
+    })
+    response.end('console.log("script loaded twice")')
+  }
+
+}).listen(1111)
+
+console.log('server listening on 1111')
--- HTTP协议原理/3-2 CORS跨域请求的限制与解决/server2.js
+++ HTTP协议原理/3-2 CORS跨域请求的限制与解决/server2.js
@@ -5,9 +5,12 @@ http.createServer(function(request,response){
   console.log('request come',request.url)
 
   response.writeHead(200,{
-    'Access-Control-Allow-Origin': 'http://127.0.0.1:8888'
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'X-Test-Cors',
+    'Access-Control-Allow-Methods': 'POST,PUT,Delete',
+    'Access-Control-Max-Age': '1000',
   })
   response.end("123")
-}).listen(8887)
+}).listen(2222)
 
-console.log('server listening on 8887')
+console.log('server listening on 2222')
